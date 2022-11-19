f1867e344867e3cfa3e7000a5b97cd8150d4de93
youruchul <youruchul@naver.com>
2022-11-19 23:11:28 +0900

    완료

--- 3. 반복문/11022.py
+++ 3. 반복문/11022.py
@@ -1,1 +1,14 @@
-#
+'''
+Case #1: 1 + 1 = 2
+Case #2: 2 + 3 = 5
+Case #3: 3 + 4 = 7
+Case #4: 9 + 8 = 17
+Case #5: 5 + 2 = 7
+'''
+import sys
+count = int(input())
+
+for i in range(1,count+1):
+    a,b = map(int,sys.stdin.readline().split())
+    #print(type(i))
+    print('Case #{}: {} + {} = {}'.format(i,a,b,(a+b)))
